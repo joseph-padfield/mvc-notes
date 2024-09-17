d7a93009125c0d44ba8959854a7ec102bb753770
JosephPadfield <josephpadfield@josephpieldsmbp.home>
2024-09-17 21:57:49 +0100

    added examples, see README notes 1 - 5

--- README.md
+++ README.md
@@ -1,25 +1,4 @@
-# Slim Framework 4 Skeleton Application
-
-Use this skeleton application to quickly setup and start working on a new Slim Framework 4 application. This application uses the latest Slim 4 with Slim PSR-7 implementation and PHP-DI container implementation along with the PHP-View template renderer. It also uses the Monolog logger.
-
-This skeleton application was built for Composer. This makes setting up a new Slim Framework application quick and easy.
-
-## Install the Application
-
-Create a new directory with your project name, e.g:
-
-
-```bash
-mkdir academyProject
-```
-
-Once inside the new directory, clone this repo:
-
-```bash
-git clone git@github.com:Mayden-Academy/slim4-skeleton.git .
-```
-
-One cloned, you must install the slim components by running:
+#Info
 
 ```bash
 composer install
@@ -35,4 +14,24 @@ Run this command in the application directory to run the test suite
 composer test
 ```
 
-That's it! Now go build something cool.
+#NOTES
+
+1. Added welcome route - see templates/welcome.phtml and app/routes.php (welcome)
+	as an example: http://0.0.0.0:8080/welcome
+	
+2. Added weather route - see templates.weather.phtml and app/routes.php (weather)
+	as an example: http://0.0.0.0:8080/weather/Bath/rainy
+	
+3. Added temperature route - see app/routes.php (temperature)
+	note the content renders directly from the function itself.
+	as an example http://0.0.0.0:8080/temperature/London/hot
+	
+4. Added pet route, demonstrating how you can use `[]` to indicate
+	that the route parameter is optional. Again, this renders directly from the function.
+	as an example http://0.0.0.0:8080/pet/Nelly
+	
+5. Added myName route. see templates/myName.phtml adn app/routes.php (myName)
+	this is an example of passing in the route parameters as an associative array.
+	as an example http://0.0.0.0:8080/myName/Jo/Padfield
+	
+
--- app/routes.php
+++ app/routes.php
@@ -5,6 +5,8 @@ use App\Controllers\CoursesAPIController;
 use Slim\App;
 use Slim\Views\PhpRenderer;
 use Slim\Interfaces\RouteCollectorProxyInterface as Group;
+use Slim\Http\ServerRequest;
+use Slim\Http\Response;
 
 return function (App $app) {
     $container = $app->getContainer();
@@ -18,4 +20,37 @@ return function (App $app) {
 
     $app->get('/courses', CoursesAPIController::class);
 
+    $app->get('/welcome', function (ServerRequest $request, Response $response, $args) use ($container)
+    {
+        $renderer = $container->get(PhpRenderer::class);
+        return $renderer->render($response, "welcome.phtml", $args);
+    });
+
+//    the following example renders the content into a file - weather.phtml
+    $app->get('/weather/{city}/{condition}', function (ServerRequest $request, Response $response, $args) use ($container)
+    {
+        $renderer = $container->get(PhpRenderer::class);
+        return $renderer->render($response, "weather.phtml", ['city' => $args['city'], 'condition' => $args['condition']]);
+    });
+
+//    the following example gives the same result but writes the html response directly within itself
+//    instead of rendering via something like weather.phtml
+    $app->get('/temperature/{city}/{condition}', function (ServerRequest $request, Response $response, $args)
+    {
+        $response->getBody()->write("<h2>The temperature in " . $args['city'] . " is " . $args['condition'] . "</h2>");
+        return $response;
+    });
+
+    $app->get('/pet/{name}[/{species}]', function (ServerRequest $request, Response $response, $args) use ($container)
+    {
+        $species = $args['species'] ?? 'pet';
+        $response->getBody()->write("<h2> I have a " . $species . " called " . $args['name'] . "</h2>");
+        return $response;
+    });
+
+    $app->get('/myName/{firstName}/{lastName}', function (ServerRequest $request, Response $response, $args) use ($container)
+    {
+        $renderer = $container->get(PhpRenderer::class);
+        return $renderer->render($response, "myName.phtml", ['firstName' => $args['firstName'], 'lastName' => $args['lastName']]);
+    });
 };
